e7e30972d632110732ce0323c8b4fa84bd0f57ec
Molly Arhammar <mollyarhammar@gmail.com>
2019-03-11 09:37:08 +0100

    clean up code

--- TwitterSearch/twitter-scrape.js
+++ TwitterSearch/twitter-scrape.js
@@ -1,8 +1,8 @@
 // Imports
 const fs = require('fs')
 const moment = require('moment')
+moment.locale('sv')
 const Twitter = require('./twitter')
-const log = require('simple-node-logger').createSimpleLogger('twitter-scrape.log')
 
 // Variables
 const secrets = require('./secrets')
@@ -18,47 +18,45 @@ const queries = [
     query: 'trump place:Denver',
     fromDate: '201610010000',
     toDate: '201611070000'
+  },
+  {
+    query: 'clinton place:Columbus',
+    fromDate: '201610010000',
+    toDate: '201611070000'
+  },
+  {
+    query: 'trump place:Columbus',
+    fromDate: '201610010000',
+    toDate: '201611070000'
   }
 ]
 
-  const client = new Twitter({
-    twitter_search_path: secrets.customSearchPath,
-    bearer_token: secrets.bearerToken
-  })
+const client = new Twitter({
+  twitter_search_path: secrets.customSearchPath,
+  bearer_token: secrets.bearerToken
+})
 
-  moment.locale('sv')
-
-queries2.forEach((query) => {
-  log.info('will be getting tweets for query')
-  log.info(query)
+queries.forEach((query) => {
   getTweets(query)
     .then((message) => {
-      log.info('got tweets with message:')
-      log.info(message)
+      console.log(message)
     })
     .catch((err) => {
-      log.info('did not get tweets, instead error:')
-      log.info(err);
+      console.log(err)
     })
 })
 
 
 function getTweets(query) {
-  log.info('inside getTweets')
   return new Promise((resolve, reject) => {
-    log.info('search client')
-    client.search( query , { method: 'post', max_pages: 1 } )
+    client.search( query , { method: 'post', max_pages: 12 } )
     .then((tweets) => {
-      log.info('got return of tweets:')
       return saveTweets(tweets, query)
     })
     .then((result) => {
-      log.info('result of saving tweets:')
-      log.info(result)
       resolve(result)
     })
     .catch((error) => {
-      log.info('error in retrieving or saving tweets:')
       reject(error)
     })
   })
@@ -66,23 +64,17 @@ function getTweets(query) {
 
 function saveTweets(tweets, query) {
   return new Promise((resolve, reject) => {
-    log.info('saving tweets')
     const timestamp = moment().format('L')
     const filename = `${timestamp}.json`
     const filepath = `./data/twitter/${month}-${query.query}-${filename}`
     const data = JSON.stringify(tweets)
-    log.info('all variables set for saving')
 
     fs.writeFile(filepath, data, 'utf8',
       (err) => {
-        log.info('callback from saved')
         if (err) {
-          log.info('got error from saving:')
-          log.info(err)
-          reject(err);
+          reject(err)
         } else {
-          log.info('succeeded in saving, resolving')
-          resolve(`Successfully saved ${filename}`);
+          resolve(`Successfully saved ${filename}`)
         }
       }
     )
--- TwitterSearch/twitter.js
+++ TwitterSearch/twitter.js
@@ -1,7 +1,5 @@
 // Imports
-const extend = require('deep-extend')
 const request = require('request-promise')
-const log = require('simple-node-logger').createSimpleLogger('twitter.log')
 
 // Class
 class Twitter {
@@ -19,20 +17,10 @@ class Twitter {
 
     this.requests = 0
     this.RATE_LIMIT = 22
-
-    log.info('created twitter request thing with options')
-    log.info(this.options)
   }
 
   search(params, opts) {
-    log.info('searching twitter with params')
-    log.info(params)
-    log.info('and opts')
-    log.info(opts)
-
     this.requests += 1
-
-    log.info('number of requests made: ' + this.requests)
 
     if (!opts.page) {
       opts.page = 1
@@ -41,69 +29,38 @@ class Twitter {
     let options = this.options
     options.body = params
 
-    log.info('will be searching with options')
-    log.info(options)
-
     return new Promise((resolve, reject) => {
       let tweets = []
-      log.info('returning promise')
-
-      log.info('requesting now')
       request.post(options)
       .then((response) => {
-        log.info('returned from request')
-        log.info('with response')
         
         if (response.error) {
-          log.info('bad request')
-          log.info('Error: ' + response.error.message)
           reject(new Error('Error: ' + response.error.message))
         }
 
-        log.info('extending tweets')
         tweets.push(response.results)
-        log.info('how many tweets? ' + response.results.length)
-
-        log.info('does data have next?')
-        log.info(response.next)
-
-        log.info('do we want more pages?')
-        log.info(opts.page <= opts.max_pages)
 
         if (response.next && opts.page < opts.max_pages) {
-          log.info('data had next and we wanted more pages')
           options.body.next = response.next
           opts.page += 1
 
-          log.info('are we below the rate limit?')
-          log.info('requests: ' + this.requests)
-          log.info('rate limit: ' + this.RATE_LIMIT)
-
           if (this.requests < this.RATE_LIMIT) {
-            log.info('we were below the rate limit, searching again')
             return this.search(params, opts)
           } else {
-            log.info('we were above the rate limit, setting timeout to search again')
             setTimeout(() => {
-              log.info('timeout activated, searching again now')
               requests = 0
               return this.search(params, opts)
             }, 65000)
           }
         } else {
-          log.info('no more pages wanted, resolving with tweets:')
           resolve(tweets)
         }
       })
       .then((result) => {
-        log.info('returned from searching again, extending tweets')
         tweets.push(result)
-        log.info('resolving with new tweets:')
         resolve(tweets)
       })
       .catch((error) => {
-        log.info('got error in searching, will reject with it:')
-        log.info(error)
         reject(error)
       })
     })
